f52632b7e0bf69c6af97ae44c809fadb0c18467d
Yang Zou <dcwx1016@gmail.com>
2021-01-01 18:05:04 +0000

    Update MazeBreadthFirstPaths.java

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,8 +1,7 @@
 package lab11.graphs;
 
-
-
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -14,58 +13,54 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
     private int s;
     private int t;
-    private boolean found;
+    private boolean targetFound = false;
     private Maze maze;
+    private Queue<Integer> fringe;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         maze = m;
-        s = m.xyTo1D(sourceX, sourceY);
-        t = m.xyTo1D(targetX, targetY);
-        found = false;
+        s = m.xyTo1D(sourceX,sourceY);
+        t = m.xyTo1D(targetX,targetY);
+        fringe = new ArrayDeque<>();
+        fringe.offer(s);
         distTo[s] = 0;
         edgeTo[s] = s;
+        marked[s] = true;
         // Add more variables here!
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs(int s) {
-        Queue<Integer> fringe = new ArrayDeque<>();
-        marked[s] = true;
-        fringe.add(s);
-        announce();
-
-        while(!fringe.isEmpty() && !found) {
-            int v = fringe.remove();
-
-            if (v == t) {
-                found = true;
-                break;
+    private void bfs() {
+        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        // mark first then loop.
+        while(!fringe.peek().equals(null)){
+            int x = fringe.poll();
+            announce();
+            if (x == t){
+                targetFound = true;
             }
-
-            for (int w : maze.adj(v)) {
-                if (!marked[w]) {
-                    edgeTo[w] = v;
+            if (targetFound){
+                return;
+            }
+            for (int w: maze.adj(x)){
+                if(!marked[w]){
                     marked[w] = true;
                     fringe.add(w);
+                    edgeTo[w] = x;
                     announce();
-                    distTo[w] = distTo[v] + 1;
-
-
+                    distTo[w] = distTo[x] + 1;
                 }
-
             }
         }
-
-
     }
 
 
     @Override
     public void solve() {
-        bfs(s);
+         bfs();
     }
 }
+
