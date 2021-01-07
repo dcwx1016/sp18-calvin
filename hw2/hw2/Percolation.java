f34c1760b7e94a53a75b9ffe4d5e9a85578e26a1
Yang Zou <dcwx1016@gmail.com>
2021-01-07 00:09:02 +0000

    update hw2 - modify neighbours & check full

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -8,106 +8,124 @@ public class Percolation {
     private int num;
     private int size;
 
-    public Percolation(int N){
+    public Percolation(int N) {
         // create N-by-N grid, with all sites initially blocked
-        if(N>0){
+        if (N > 0) {
             num = N;
-            sites = new int[N*N];
-            groups = new WeightedQuickUnionUF(N*N);
-            for (int i = 0; i < N; i++){
-                for (int j = 0; j < N; j++){
+            sites = new int[N * N];
+            groups = new WeightedQuickUnionUF(N * N);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
                     sites[i * N + j] = 0;
                 }
             }
-        }else{
+        } else {
             throw new java.lang.IllegalArgumentException("Please input a positive N.");
         }
 
 
     }
-    public void open(int row, int col){
+    public void open(int row, int col) {
         // open the site (row, col) if it is not open already
-        if( row>=0 && col >= 0 && row<num && col<num){
-            if(!isOpen(row,col)){
+        if (row >= 0 && col >= 0 && row < num && col < num) {
+            if (!isOpen(row, col)) {
                 int middle = row * num + col;
                 sites[middle] = 1;
-                int[] neighbor = new int[]{middle - 1,middle + 1,middle - num, middle + num};
-                for (int w: neighbor){
-                    if (w/num < num && w/num >=0 && w%num < num && w%num >= 0){
-                        if (isOpen(w/num,w%num)){
-                            groups.union(middle,w);
-                        }
+                int[] neighbor;
+                if (middle == 0) {
+                    neighbor = new int[]{middle + 1, middle + num};
+                } else if (middle == num - 1) {
+                    neighbor = new int[]{middle - 1, middle + num};
+                } else if (middle == num * (num - 1)) {
+                    neighbor = new int[]{middle + 1, middle - num};
+                } else if (middle == num * num - 1) {
+                    neighbor = new int[]{middle - 1, middle - num};
+                } else if (col == 0) {
+                    neighbor = new int[]{middle + 1, middle - num, middle + num};
+                } else if (col == num - 1) {
+                    neighbor = new int[]{middle - 1, middle - num, middle + num};
+                } else if (row == 0) {
+                    neighbor = new int[]{middle - 1, middle + 1, middle + num};
+                } else if (row == num - 1) {
+                    neighbor = new int[]{middle - 1, middle + 1, middle - num};
+                } else {
+                    neighbor = new int[]{middle - 1, middle + 1, middle - num, middle + num};
+                }
+
+                for (int w: neighbor) {
+                    if (isOpen(w / num, w % num)) {
+                        groups.union(middle, w);
                     }
                 }
                 size += 1;
             }
-        }else{
+        } else {
             throw new java.lang.IndexOutOfBoundsException("Please input valid row or/and col.");
         }
-
-
     }
-    public boolean isOpen(int row, int col)  {
+
+    public boolean isOpen(int row, int col) {
         // is the site (row, col) open?
-        if( row>=0 && col >= 0 && row<num && col<num){
-            if (sites[row * num + col] == 1){
+        if (row >= 0 && col >= 0 && row < num && col < num) {
+            if (sites[row * num + col] == 1) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
-        }else{
+        } else {
             throw new java.lang.IndexOutOfBoundsException("Please input valid row or/and col.");
         }
 
     }
-    public boolean isFull(int row, int col){
+    public boolean isFull(int row, int col) {
         // is the site (row, col) full?
-        if(row>=0 && col >= 0 && row<num && col<num){
-            if (!isOpen(row, col)){
+        if (row >= 0 && col >= 0 && row < num && col < num) {
+            if (!isOpen(row, col)) {
                 return false;
             }
-            for (int i = 0; i < num; i++){
-                if (isOpen(0, i)){
-                    if (groups.connected(i,row * num + col)){
+            for (int i = 0; i < num; i++) {
+                if (isOpen(0, i)) {
+                    if (groups.connected(i, row * num + col)) {
                         return true;
                     }
                 }
             }
             return false;
-        }else{
+        } else {
             throw new java.lang.IndexOutOfBoundsException("Please input valid row or/and col.");
         }
 
     }
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         return size;
     }
 
-    public boolean percolates(){
+    public boolean percolates() {
         // does the system percolate?
-        for (int i = 0; i < num; i++){
-            for (int j = 0; j < num; j++){
-            if (groups.connected(num*(num-1)+i, j)){
-                return true;
-            }}
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (groups.connected(num * (num - 1) + i, j)) {
+                    return true;
+                }
+            }
         }
         return false;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // use for unit testing (not required)
         Percolation a = new Percolation(5);
-        a.open(1,1);
-        a.open(1,0);
-        for (int i = 0; i < 5; i++){
-            for (int j = 0; j < 5; j++){
-                System.out.println(a.sites[i* a.num + j]);
+        a.open(1, 1);
+        a.open(1, 0);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(a.sites[i * a.num + j]);
             }
         }
-        System.out.println(a.groups.connected(2,3));
-        System.out.println(a.groups.connected(0,2));
+        System.out.println(a.groups.connected(2, 3));
+        System.out.println(a.groups.connected(0, 2));
         System.out.println(a.percolates());
-        a.open(0,0);
+        a.open(0, 0);
         System.out.println(a.percolates());
-
-    }   // use for unit testing (not required)
+    }
 }
